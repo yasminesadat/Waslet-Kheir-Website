0b48c398a58d87d762103966059ea20fdb4cf09e
hanaseif19 <hana.seiff19@gmail.com>
2024-04-28 23:13:46 +0300

    registerdonorpage draft2

---
+++ WasletKheir/wasletkheir/src/components/MapTrial.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const GoogleMap = () => {
+    return (
+
+        <iframe
+            title="google map"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3453.093543714628!2d31.47535887549011!3d30.062853017698387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145819abf3cb2013%3A0xa3ef9e387e234105!2sGroup%2044!5e0!3m2!1sen!2seg!4v1714325706970!5m2!1sen!2seg"
+            width="70%"
+            height="70%"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            alignItems="center"
+            referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+
+        // </div>
+    );
+};
+
+export default GoogleMap;
--- WasletKheir/wasletkheir/src/components/RegisterDonor.js
+++ WasletKheir/wasletkheir/src/components/RegisterDonor.js
@@ -1,34 +1,75 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from 'react';
+import GoogleMap from "./MapTrial";
 
 
 function RegisterDonor() {
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
     const [isRegistered, setIsRegistered] = useState(false);
+    const [selectedOption, setSelectedOption] = useState('');
+    const [showUploadDr, setShowUploadDr] = useState(false);
+    const [showUploadTeacher, setShowUploadTeacher] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const [showMessage, setShowMessage] = useState(false);
 
 
-    function handleClick(event) {
+    const openPopUp = () => {
+        setIsOpen(true);
+        setShowMessage(false);
+    };
+
+    const closePopUp = () => {
+
+        setShowMessage(true);
+        setTimeout(() => {
+            setIsOpen(false);
+
+        }, 2000);
+
+
+
+
+    };
+
+    const handleDonorTypeChoice = (event) => {
+        console.log("i chose" + event.target.value);
+        const selectedValue = event.target.value;
+        setSelectedOption(selectedValue);
+
+        if (selectedValue === 'Doctor') {
+            setShowUploadDr(true);
+            setShowUploadTeacher(false);
+
+        } else if (selectedValue === 'Teacher') {
+            setShowUploadTeacher(true);
+            setShowUploadDr(false);
+        } else {
+            setShowUploadTeacher(false);
+            setShowUploadDr(false);
+        }
+    };
+    const handleMapClick = () => {
+        setShowMessage(true);  // Location pinned successfully
+        console.log("map");
+    };
+    const handleClick = (event) => {
         setIsLoading(true);
-        event.preventDefault(); // is this necessary?
+        event.preventDefault();
         setTimeout(() => {
             setIsRegistered(true);
-
             setIsLoading(false);
-
             setTimeout(() => {
                 navigate("/OrgMain");
-
             }, 2000);
         }, 2000);
-    }
-
-
-
+    };
 
     return (
-        <div className="RegisterDonor">
-            <form className="regform" onSubmit={handleClick}>
+
+        <div className="RegisterDonor" style={{ maxHeight: 'calc(100vh - 100px)' }}>
+            <form className="regform" onSubmit={handleClick} style={{ overflowY: 'auto', maxHeight: '100%' }}>
+                <h1 className='title'> Create an Account</h1>
                 <div className="formElement">
                     <label htmlFor="fname"><b>First Name</b></label>
                     <input type="text" placeholder="Enter your first name" name="fname" id="fname" required />
@@ -62,22 +103,51 @@ function RegisterDonor() {
                 </div>
                 <div className="formElement">
                     <label htmlFor="add"><b>Address</b></label>
-                    <input type="text" placeholder="Enter your area and governate" name="add" id="add" required />
+                    <input type="text" placeholder="Enter your address" name="add" id="add" required />
+                </div>
+                <div className="formElement">
                     <input type="text" placeholder="Enter your area" name="area" id="area" required />
+                </div>
+                <div className="formElement">
                     <input type="text" placeholder="Enter your governate" name="gov" id="gov" required />
                 </div>
                 <div className="formElement">
                     <label htmlFor="role"><b>Choose a role</b></label>
-                    <select name="role" id="role">
+                    <select value={selectedOption} onChange={handleDonorTypeChoice}>
                         <option value="reg">Regular Donor</option>
-                        <option value="dr">Doctor</option>
-                        <option value="teach">Teacher</option>
+                        <option value="Doctor">Doctor</option>
+                        <option value="Teacher">Teacher</option>
                     </select>
                 </div>
+                {showUploadDr && (
+                    <div className="formElement">
+                        <label htmlFor="fileInput"><b>Upload your Medical License</b></label>
+                        <input type="file" />
+                    </div>
+                )}
+                {showUploadTeacher && (
+                    <div className="formElement">
+                        <label htmlFor="fileInput"><b>Upload your Teaching License</b></label>
+                        <input type="file" />
+                    </div>
+                )}
                 <div className="formElement">
-                    <button type="button" onClick={handleClick} disabled={isLoading || isRegistered}
-                        className={isRegistered ? 'greenButton' : ''}
-                    >
+                    <button type="button" className='NormalButton' onClick={openPopUp}>
+                        Pin Location on Map
+                    </button>
+                    {isOpen && (
+                        <div className="popup">
+                            <div className="popup-content"  >
+                                <GoogleMap />
+                                <button className='NormalButton' onClick={closePopUp}>Close</button>
+                                {showMessage && (<h3>Pinned successfully! </h3>)}
+                            </div>
+
+                        </div>
+                    )}
+                </div>
+                <div className="formElement">
+                    <button type="submit" disabled={isLoading || isRegistered} className={isRegistered ? 'greenButton' : 'NormalButton'}>
                         {isLoading ? 'Loading...' : isRegistered ? 'Registered!' : 'Register'}
                     </button>
                 </div>
@@ -86,5 +156,4 @@ function RegisterDonor() {
     );
 }
 
-// add google maps and uplaoding documents idea 
 export default RegisterDonor;
