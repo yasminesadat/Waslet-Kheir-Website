7b8542da45e12feae34be13634adb9500a8a6e05
Yasmine Elsadat <yasmine.atef2004@gmail.com>
2024-05-09 18:22:59 +0300

    navbarChange+LoginStylesAdjusted

--- WasletKheir/wasletkheir/src/components/Admin/Login.js
+++ WasletKheir/wasletkheir/src/components/Admin/Login.js
@@ -22,7 +22,6 @@ function handleSubmit(e) {
   return (
     <div>
     <form onSubmit={handleSubmit} className="login">
-        <div className="login-container"></div>
         <div className="input-group">
             <input required type="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} className="input" autoComplete="off"/>
             <label className="user-label">Username</label>
@@ -34,18 +33,13 @@ function handleSubmit(e) {
         <input type="checkbox" id="rememberMe"/>
         <label>Remember me</label>
         </div>
-        <button type="submit">Login</button>
-
-    
+        <button type="submit">Login</button>   
     </form>
  
-    <div className="register-account">
-        <div className='go-to-admin' >
-        <a href="/">Login as a user</a>
-    </div>
+    <div className="user-option">
+        <a href="/">Login as a User</a>
     </div>
    
-
     <img src="https://i.ibb.co/gFxywzc/logo1Yes.png" alt="Waslet Kheir" className="welcome-page-logo" />
     <div className="vertical-line"></div>
     <div className="left-side-image">
