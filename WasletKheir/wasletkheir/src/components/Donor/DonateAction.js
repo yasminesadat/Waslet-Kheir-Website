ba502d652cb3fb8b62b936e12bd729bc232e9383
diana <daiana.rehan.dr@gmail.com>
2024-05-10 03:46:40 +0300

    donate page

--- WasletKheir/wasletkheir/src/components/Donor/DonateAction.js
+++ WasletKheir/wasletkheir/src/components/Donor/DonateAction.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import DonorNavbar from './DonorNavbar';
-import { route,useLocation } from 'react-router-dom';
+import {useLocation } from 'react-router-dom';
 import ClothesCard from './ClothesCard';
+import { Select } from 'antd';
+import { Calendar } from "antd";
 
+const customStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      width: 200, // Adjust the width as needed
+      marginTop: '5%',
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isSelected ? '#007bff' : 'white',
+      color: state.isSelected ? 'white' : '#000',
+      '&:hover': {
+        backgroundColor: '#007bff',
+        color: 'white',
+      },
+    }),
+  };
 
 export default function DonateAction() {
     const location = useLocation();
@@ -19,8 +37,24 @@ export default function DonateAction() {
     return (
         <div>
             <DonorNavbar />
-            <div>
-            {renderCard()}
+            <br/>
+            <br/>
+            <br/>
+
+            <div style={{marginTop:'5%'}}>
+                <span><form >
+                    <label>Select the transportation</label>
+                    <Select styles={customStyles}>      
+                    <option value="Option0">Vehicle Type</option>
+                    <option value="Option1">Truck</option>
+                    <option value="Option2">Car</option>
+                    <option value="Option3">Motorcycle</option>
+                    </Select>
+                    <label>Select the suitable timing</label>
+             
+                    </form></span>
+                    <span>
+            {renderCard()}</span>
             </div>
         </div>
     );
